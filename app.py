b0dd285652d6789c75c6e1eac9c4985cb026c5c8
mmiskatul <masabimiskat@gmail.com>
2025-11-26 19:30:41 +0600

    import the streamlit and use saved mode to visulize the result

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st 
+import pandas as pd 
+import joblib 
+
+
+model =joblib.load("Decision_tree_credit_model.pkl") 
+encoders ={col:joblib.load(f"{col}_encoder.pkl") for col in ["Sex","Housing","Saving accounts","Checking accounts"]} 
+
+st.title("Credit Risk Prediction App") 
+st.write("Enter applicant information to predict if the credit risk is good or bad ") 
+
+age =st.number_input("Age",min_value=18,max_value=80,value=30) 
+sex =st.selectbox("Sex",["male","Female"]) 
+
+Job =st.number_input("Job (0-3)",min_value=0,max_value=3,value=1) 
+housing =st.selectbox("Housing",["Own","Rent","Free"])
+saving_account =st.selectbox("Saving Accounts",["little","moderate","rich","quite rich"])
+Checking_account =st.selectbox("Checking Accounts",["little","moderate","rich","quite rich"])
+credit_amount =st.number_input("Credit Amount",min_value=0,value=1000) 
+duration =st.number_input("Duration (month)",min_value=1,value=12) 
